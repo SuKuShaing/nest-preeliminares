d4a7141c66f18703ff374d18fecd736b1321db3f
Sebastian <sebastian.sanhueza.t@usach.cl>
2026-03-23 14:37:10 -0300

    getters, métodos y this

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,12 +1,27 @@
 export class Pokemon {
     public readonly id: number;
     public name: string;
+    // public imageUrl: string;
 
     constructor(id: number, name: string) {
         console.log("Llamado al Constructor");
 
         this.id = id;
         this.name = name;
+        // this.imageUrl = imageUrl;
+    }
+
+    get imageUrl(): string {
+        // funciona como un parametro más
+        return `https://pokemon.com/${this.id}.jpg`;
+    }
+
+    public scream() {
+        console.log(`${this.name.toUpperCase()}!!!!!!`);
+    }
+
+    private speak() {
+        console.log(`${this.name}, ${this.name}, ${this.name}`);
     }
 }
 
@@ -26,3 +41,8 @@ export const charmander = new Pokemon(4, "Charmander");
 
 // charmander.id = 10; No se permiten modificaciones gracias al read only
 charmander.name = "Mew";
+
+console.log(charmander);
+console.log(charmander.imageUrl);
+charmander.scream();
+// charmander.speak(); // no se puede ocupar fuera puesto que es private
